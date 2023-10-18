f456f9e991a01378f9b1bd5053e23ca58fe4408a
caleblee12694 <caleblee12694@gmail.com>
2023-10-18 13:32:15 -0400

    Added branch 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +22,25 @@ function App() {
   );
 }
 
+export default App;*/
+
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import Kanbas from "./Kanbas";
+import {HashRouter} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router";
+
+function App() {
+  return (
+    <HashRouter>
+      <div>
+      <Routes>
+          <Route path="/hello"    element={<HelloWorld/>}/>
+          <Route path="/Labs/*"   element={<Labs/>}/>
+          <Route path="/Kanbas/*" element={<Kanbas/>}/>
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
 export default App;
