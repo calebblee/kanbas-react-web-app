8b606e05255766df261ea5dc7c417c45b49e531c
Caleb Lee <caleblee@Calebs-MacBook-Pro.local>
2023-10-20 00:47:43 -0400

    Added a3 stuff

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,11 +1,22 @@
-import Nav from "../Nav";
+import KanbasNavigation from "./KanbasNavigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Courses from "./Courses";
 
 function Kanbas() {
    return (
-      <div>
-         <Nav />
-         <h1>Kanbas</h1>
+      <div className="d-flex">
+         <KanbasNavigation />
+         <div>
+            <Routes>
+               <Route path="/" element={<Navigate to="Dashboard" />} />
+               <Route path="Account" element={<h1>Account</h1>} />
+               <Route path="Dashboard" element={<Dashboard />} />
+               <Route path="Courses/*" element={<h1>Courses</h1>} />
+               <Route path="Courses/:courseID/*" element={<Courses />} />
+            </Routes>
+         </div>
       </div>
    );
 }
-export default Kanbas
+export default Kanbas;
