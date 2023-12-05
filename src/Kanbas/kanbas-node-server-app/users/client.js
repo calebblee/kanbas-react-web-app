49967c99e3b10c30eb1455b446f207905986e9b3
Caleb Lee <caleblee@Calebs-MacBook-Pro.local>
2023-12-04 23:31:06 -0500

    useless

--- src/Kanbas/kanbas-node-server-app/users/client.js
+++ src/Kanbas/kanbas-node-server-app/users/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_API = "http://localhost:4000";
+export const BASE_API = "https://kanbas-node-server-app-mnvx.onrender.com";
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
     const response = await request.post(`${USERS_API}/signin`, credentials);
